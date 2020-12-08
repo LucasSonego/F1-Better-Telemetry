43439abfea9a7efcc63bcf4e3bc9d19bd17f328d
Lucas Sônego <sonego_lucas@outlook.com>
2020-12-08 15:29:54 -0300

    :sparkles: feat: Storing player car id

--- client/index.js
+++ client/index.js
@@ -7,8 +7,26 @@ const F1Telemetry = new F1TelemetryClient({ bigintEnabled: false });
 
 const { PACKETS } = constants;
 
+let playerCarId = null;
+
 server.on("connection", socket => {
   console.log("New client connected");
+
+  socket.on("playerCarId", data => {
+    server.sockets.emit("playerCarId", data);
+  });
+});
+
+const io = require("socket.io-client");
+const client = io.connect("http://localhost:4000");
+
+F1Telemetry.on(PACKETS.participants, data => {
+  if (data && data.m_header.m_playerCarIndex !== playerCarId) {
+    playerCarId = data.m_header.m_playerCarIndex;
+    client.emit("playerCarId", data.m_header.m_playerCarIndex);
+    console.log("Player car id: " + playerCarId);
+  }
+});
 });
 
 F1Telemetry.start();
