ecb6bf86aeea44c9c414f1b5b7a0661d23fcfbfd
Lucas Sônego <sonego_lucas@outlook.com>
2020-12-14 14:13:47 -0300

    :sparkles: feat: Showing the lap times

--- webapp/src/Components/LapData/index.jsx
+++ webapp/src/Components/LapData/index.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "./styles";
 
-import testLapData from "../../exampleData/LapData";
+// import testLapData from "../../exampleData/LapData";
 
 function LapData({ telemetry }) {
-  const [laps, setLaps] = useState(testLapData);
-  // const [laps, setLaps] = useState();
+  // const [laps, setLaps] = useState(testLapData);
+  const [laps, setLaps] = useState();
 
   useEffect(() => {
     telemetry.once("lapData", data => {
@@ -47,8 +47,95 @@ function LapData({ telemetry }) {
     });
   }, [laps, telemetry]);
 
+  function formattedTime(time) {
+    let minutes = Math.floor(Math.floor(time / 60));
+    if (!minutes) {
+      return Number(time).toFixed(3);
+    }
+    let remainingTime = (time - minutes * 60).toFixed(3);
+    if (remainingTime < 10) {
+      remainingTime = `0${remainingTime}`;
+    }
+
+    return `${minutes}:${remainingTime}`;
+  }
+
   return (
     <Container>
+      {laps &&
+        laps
+          .slice(0)
+          .reverse()
+          .map((lap, index) => (
+            <div className="lap" key={index}>
+              <div className="general">
+                <div className="lap-number">
+                  <span>{lap.m_currentLapNum}</span>
+                </div>
+                <div className="lap-time">
+                  <span>
+                    {lap.m_lapTime ? (
+                      <>
+                        {formattedTime(lap.m_lapTime)}
+                        {"   "}
+                        {lap.m_lastLapTime ? (
+                          <>
+                            {lap.m_lastLapTime < lap.m_lapTime ? (
+                              <div className="diff red">
+                                +
+                                {formattedTime(
+                                  lap.m_lapTime - lap.m_lastLapTime
+                                )}
+                              </div>
+                            ) : (
+                              <div className="diff green">
+                                -
+                                {formattedTime(
+                                  lap.m_lastLapTime - lap.m_lapTime
+                                )}
+                              </div>
+                            )}
+                          </>
+                        ) : (
+                          <div className="diff neutral">+0.000</div>
+                        )}
+                      </>
+                    ) : (
+                      "--:--"
+                    )}
+                  </span>
+                </div>
+              </div>
+              <hr />
+              <div className="sectors">
+                {lap.m_sector1TimeInMS ? (
+                  <span className="time">
+                    {formattedTime(lap.m_sector1TimeInMS / 1000)}
+                  </span>
+                ) : (
+                  <span className="sector">S1</span>
+                )}
+                <span>
+                  {lap.m_sector2TimeInMS ? (
+                    <span className="time">
+                      {formattedTime(lap.m_sector2TimeInMS / 1000)}
+                    </span>
+                  ) : (
+                    <span className="sector">S2</span>
+                  )}
+                </span>
+                <span>
+                  {lap.m_sector3TimeInMS ? (
+                    <span className="time">
+                      {formattedTime(lap.m_sector3TimeInMS / 1000)}
+                    </span>
+                  ) : (
+                    <span className="sector">S3</span>
+                  )}
+                </span>
+              </div>
+            </div>
+          ))}
     </Container>
   );
 }
