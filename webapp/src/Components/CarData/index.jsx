d59421aec1c10e6f83fe2cd2651a769f199c94b5
Lucas Sônego <sonego_lucas@outlook.com>
2021-01-06 22:07:30 -0300

    :sparkles: feat: Car

--- webapp/src/Components/CarData/index.jsx
+++ webapp/src/Components/CarData/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Container } from "./styles";
+import Car from "../Car";
 function CarData({ carStatus, carTelemetry }) {
   return (
     <Container>
+        <Car
+          frontWingDamage={{
+            left: carStatus?.m_frontLeftWingDamage,
+            right: carStatus?.m_frontRightWingDamage,
+          }}
+          rearWingDamage={carStatus?.m_rearWingDamage}
+          tyreTemperatures={carTelemetry?.m_tyresInnerTemperature}
+          brakeTemperatures={carTelemetry?.m_brakesTemperature}
+          engineTemperature={carTelemetry?.m_engineTemperature}
+          gearboxDamage={carStatus?.m_gearBoxDamage}
+          drsFault={carStatus?.m_drsFault}
+        />
     </Container>
   );
 }
